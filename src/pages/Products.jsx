1d4990286f52507bdc2966f05c883fe92d12af8c
unknown <skshihab170@gmail.com>
2024-08-17 17:25:57 +0600

    4th

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,9 +4,9 @@ import { useEffect, useState } from "react";
 const Products = () => {
 const [data,setData]=useState()
 
-useEffect(
+// useEffect(
     
-,[] )
+// ,[] )
     
     return (
         <div>
